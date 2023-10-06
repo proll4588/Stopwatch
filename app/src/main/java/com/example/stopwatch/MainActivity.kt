fc2f76b688f42e741f2e89048bfae1cc59873164
Kostya <prol4588@gmail.com>
2023-10-06 13:03:46 +0700

    add landscape orientation

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -11,11 +11,25 @@ class MainActivity : AppCompatActivity() {
     var running = false
     var offset: Long = 0
 
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            } else setBaseTime()
+        }
+
         val startButton = findViewById<Button>(R.id.start_button)
         startButton.setOnClickListener {
             if (!running) {
@@ -41,10 +55,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
     fun setBaseTime() {
         stopwatch.base = SystemClock.elapsedRealtime() - offset
     }
-    
+
     fun saveOffset() {
         offset = SystemClock.elapsedRealtime() - stopwatch.base
     }
